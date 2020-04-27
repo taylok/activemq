cfe7d32f43d6f3150dbf3604bc3425c089b1c670
Keith <taylor.km61@icloud.com>
2020-04-27 21:20:34 +0100

    Sending messages to queues with QueueSession interface

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # activemq
 
 0.1.0 Sending messages to queues with Session interface
+0.1.1 Sending messages to queues with QueueSession interface
--- module3/src/main/java/com/pluralsight/practicaljms/Application.java
+++ module3/src/main/java/com/pluralsight/practicaljms/Application.java
@@ -1,4 +1,4 @@
-package com.pluarlsight.practicaljms;
+package com.pluralsight.practicaljms;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 
@@ -6,10 +6,17 @@ import javax.jms.*;
 
 /**
  *  0.1.0 Uses Generic Session Interface
+ *  0.1.1 Uses QueueSession Interface
  */
 public class Application {
 
     public ConnectionFactory createConnectionFactory() {
+        return new ActiveMQConnectionFactory(
+                "tcp://localhost:61616"
+        );
+    }
+
+    public QueueConnectionFactory createQueueConnectionFactory() {
         return new ActiveMQConnectionFactory(
                 "tcp://localhost:61616"
         );
@@ -20,17 +27,27 @@ public class Application {
         return cf.createConnection();
     }
 
+    public QueueConnection createQueueConnection(QueueConnectionFactory cf)
+            throws JMSException {
+        return cf.createQueueConnection();
+    }
+
     public Session createSession(Connection connection)
             throws JMSException {
         return connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
     }
 
+    public QueueSession createQueueSession(QueueConnection connection)
+            throws JMSException {
+        return connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+    }
+
     public static void main( String... args ) throws Exception {
         Application app = new Application();
-        ConnectionFactory cf = app.createConnectionFactory();
-        Connection conn = app.createConnection(cf);
-        Session session = app.createSession(conn);
-        app.sendTextMessageToQueue("Test Message", session);
+        QueueConnectionFactory cf = app.createQueueConnectionFactory();
+        QueueConnection conn = app.createQueueConnection(cf);
+        QueueSession session = app.createQueueSession(conn);
+        app.sendTextMessageToQueue("Another Message", session);
         session.close();
         conn.close();
     }
@@ -40,6 +57,14 @@ public class Application {
         Queue queue = session.createQueue("TEST_DESTINATION");
         TextMessage msg = session.createTextMessage(message);
         MessageProducer messageProducer = session.createProducer(queue);
+        messageProducer.send(msg);
+    }
+
+    public void sendTextMessageToQueue(String message,
+                                       QueueSession session) throws JMSException {
+        Queue queue = session.createQueue("TEST_DESTINATION");
+        TextMessage msg = session.createTextMessage(message);
+        QueueSender messageProducer = session.createSender(queue);
         messageProducer.send(msg);
     }
 }
